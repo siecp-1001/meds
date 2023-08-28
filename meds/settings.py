b38e3a58f600a5d2245541974dc83765a1876ef9
siecp-1001 <108583424+siecp-1001@users.noreply.github.com>
2023-08-27 17:48:54 -0700

    run

--- meds/settings.py
+++ meds/settings.py
@@ -76,7 +76,10 @@ WSGI_APPLICATION = 'meds.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-  
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
